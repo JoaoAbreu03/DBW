e2e943555580151ec57fab3e5723576e25700367
JoaoAbreu03 <joaoabreu221@gmail.com>
2024-04-09 18:06:11 +0100

    Conncted String MONGO funcionar

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
-
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 var http = require('http').createServer(app)
 
@@ -11,9 +11,43 @@ const cors = require('cors')
 const PORT = config.port
 app.use(cors())
 
+mongoose.connect("mongodb+srv://joaoabreu221:joaoabreu@dbw.toqlem4.mongodb.net/").then( () => console.log("DB Connectada")).catch(console.error);
+
+const uri = "mongodb+srv://joaoabreu221:joaoabreu221@dbw.toqlem4.mongodb.net/?retryWrites=true&w=majority&appName=DBW";
+// const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+// function run() {
+//   console.log("teste");
+//   try {
+//     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+//     console.log("teste1");
+
+//      mongoose.connect(uri, {
+//       sslValidate: false,
+//       ssl: true,
+//       sslCert: certFileBuf,
+//       sslKey: certFileBuf,
+//       checkServerIdentity: false,
+//       sslPass: 'mysslPass',
+//   });
+//     console.log("teste11");
+
+//     mongoose.connection.db.admin().command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } 
+//   catch(error){
+//     console.error;
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     mongoose.disconnect();
+//   }
+  
+// }
+
+//run();
+
 http.listen(PORT, () =>{
     console.log(`Listening in port ${PORT}`)
-})
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/view/index.html'));
@@ -22,4 +56,3 @@ app.get('/', (req, res) => {
 app.get('/index2.html', (req, res) => {
   res.sendFile(path.join(__dirname+'/view/index2.html'));
 });
-
